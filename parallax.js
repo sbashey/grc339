a6e7b691fc61ed94de41703c6ee608da6f3cdb60
sbashey <60018378+sbashey@users.noreply.github.com>
2020-05-31 18:52:03 -0700

    Add files via upload

---
+++ parallax.js
@@ -0,0 +1,19 @@
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  $(".title").css({
+    transform: "translate(0px, " + wScroll / 2 + "%)"
+  });
+});
+
+$(window).scroll(function () {
+var wScroll = $(this).scrollTop();
+
+$(".title").css({
+transform: "translate(0px, " + wScroll / 1.5 + "%)"
+});
+
+$(".transparent").css({
+transform: "translate(0px, -" + wScroll / 3 + "%)"
+});
+
+});
